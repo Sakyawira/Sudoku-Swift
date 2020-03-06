dfeb452fe4bffc8d09dd259e0901618c76491d75
Sakyawira <sakyawira@gmail.com>
2020-03-06 17:05:50 +1300

    solve sudoku function

--- Sudoku/Sudoku/Sudoku.swift
+++ Sudoku/Sudoku/Sudoku.swift
@@ -13,7 +13,7 @@ let UNASSIGNED = 0;
 
 let myString : String = "hi";
 
-let grid : [[Int]] = [  [1, 0, 0, 0, 0, 0, 0, 0, 0],
+var grid : [[Int]] = [  [1, 0, 0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -23,7 +23,7 @@ let grid : [[Int]] = [  [1, 0, 0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0],
                         [0, 0, 0, 0, 0, 0, 0, 0, 0]];
 
-func print_grid(grid:[[Int]])
+func print_grid(/*grid:[[Int]]*/)
 {
     for x in 0..<grid.count{
         var line = "";
@@ -109,4 +109,38 @@ func is_safe(row : Int, col : Int, num : Int) -> Bool
     {
         return false;
     }
+}
+
+func solve_sudoku() -> Bool
+{
+    var row : Int = 0;
+    var col : Int = 0;
+
+    // If there is no unassigned location,
+    // we are done
+    if (!find_unassigned_location(row : &row, col : &col))
+    {
+        return true; // success!
+    }
+
+    // consider digits 1 to 9
+    for num in 1...9
+    {
+        // if looks promising
+        if (is_safe(row : row, col : col, num : num))
+        {
+            // make tentative assignment
+            grid[row][col] = num;
+
+            // return, if success, yay!
+            if (solve_sudoku())
+            {
+                return true;
+            }
+
+            // failure, unmake & try again
+            grid[row][col] = UNASSIGNED;
+        }
+    }
+    return false; // this triggers backtracking
 }
