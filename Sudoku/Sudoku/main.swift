2ab25020a6a082105020db328939d78e2b0e145e
Sakyawira <sakyawira@gmail.com>
2020-03-06 15:13:38 +1300

    init

---
+++ Sudoku/Sudoku/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sudoku
+//
+//  Created by Sakyawira Ruslim on 6/03/20.
+//  Copyright © 2020 Sakyawira Ruslim. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
