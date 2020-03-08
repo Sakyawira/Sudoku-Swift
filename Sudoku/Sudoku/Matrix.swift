57c2a6369377dcfae99dc8b8fdc346f8e4953313
Sakyawira <sakyawira@gmail.com>
2020-03-09 11:32:11 +1300

    using subscript to print grid

--- Sudoku/Sudoku/Matrix.swift
+++ Sudoku/Sudoku/Matrix.swift
@@ -10,16 +10,29 @@ import Foundation
 
 struct Matrix {
     let rows: Int, columns: Int
-    var grid: [Double]
+    var grid: [Int]
     init(rows: Int, columns: Int) {
         self.rows = rows
         self.columns = columns
-        grid = Array(repeating: 0.0, count: rows * columns)
+        grid = Array(repeating: 0, count: rows * columns)
     }
     func indexIsValid(row: Int, column: Int) -> Bool {
         return row >= 0 && row < rows && column >= 0 && column < columns
     }
-    subscript(row: Int, column: Int) -> Double {
+    func printGrid(){
+        for x in 0..<rows{
+            var line = "";
+            for y in 0..<columns
+            {
+                line += String(self[x, y]) + ", ";
+                line += "";
+            }
+            print(line);
+        }
+        print("");
+        //print(grid);
+    }
+    subscript(row: Int, column: Int) -> Int {
         get {
             assert(indexIsValid(row: row, column: column),"Index out of range");
             return grid[(row * columns) + column];
--- Sudoku/Sudoku/Sudoku.swift
+++ Sudoku/Sudoku/Sudoku.swift
@@ -10,27 +10,15 @@ import Foundation
 
 class Sudoku{
     let N : Int = 9;
-    let UNASSIGNED = 0;
+    let UNASSIGNED : Int = 0;
     
     //let myString : String = "hi";
     
-    var grid : [[Int]] = [  [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                            [0, 0, 0, 0, 0, 0, 0, 0, 0]];
+    var grid : Matrix = Matrix(rows : 9, columns: 9);
     
     func print_grid(/*grid:[[Int]]*/)
     {
-        for i in 0..<grid.count
-        {
-            print(grid[i]);
-        }
-        print("");
+        grid.printGrid();
     }
     
     func find_unassigned_location( row : inout Int, col : inout Int) -> Bool
@@ -39,7 +27,7 @@ class Sudoku{
         {
             for j in 0..<N
             {
-                if (grid[i][j] == UNASSIGNED)
+                if (grid[i,j] == UNASSIGNED)
                 {
                     row = i;
                     col = j;
@@ -57,7 +45,7 @@ class Sudoku{
     {
         for col in 0..<N
         {
-            if (grid[row][col] == num)
+            if (grid[row, col] == num)
             {
                 return true;
             }
@@ -71,7 +59,7 @@ class Sudoku{
     {
         for row in 0..<N
         {
-            if (grid[row][col] == num)
+            if (grid[row, col] == num)
             {
                 return true;
             }
@@ -87,7 +75,7 @@ class Sudoku{
         {
             for col in 0..<3
             {
-                if (grid[row + boxStartRow][col + boxStartCol] == num)
+                if (grid[row + boxStartRow, col + boxStartCol] == num)
                 {
                     return true;
                 }
@@ -100,7 +88,7 @@ class Sudoku{
     {
         /* Check if 'num' is not already placed in
          current row, current column and current 3x3 box */
-        if (!used_in_row(row : row, num : num) && !used_in_col(col : col, num : num) && !used_in_box(boxStartRow : row - row % 3, boxStartCol : col - col % 3, num : num) && grid[row][col] == UNASSIGNED)
+        if (!used_in_row(row : row, num : num) && !used_in_col(col : col, num : num) && !used_in_box(boxStartRow : row - row % 3, boxStartCol : col - col % 3, num : num) && grid[row, col] == UNASSIGNED)
         {
             return true;
         }
@@ -129,7 +117,7 @@ class Sudoku{
             if (is_safe(row : row, col : col, num : num))
             {
                 // make tentative assignment
-                grid[row][col] = num;
+                grid[row, col] = num;
                 
                 // return, if success, yay!
                 if (solve_sudoku())
@@ -138,7 +126,7 @@ class Sudoku{
                 }
                 
                 // failure, unmake & try again
-                grid[row][col] = UNASSIGNED;
+                grid[row, col] = UNASSIGNED;
             }
         }
         return false; // this triggers backtracking
@@ -151,7 +139,12 @@ class Sudoku{
         
         print(random_sequence);
         print("");
-        grid[0] = random_sequence;
+        
+        for n in 0..<9
+        {
+             grid[0, n] = random_sequence[n];
+        }
+       
         
         print_grid();
         
@@ -170,7 +163,7 @@ class Sudoku{
         {
             let randomNumber : Int = Int.random(in: 0...8);
             let randomNumber2 : Int = Int.random(in: 0...8);
-            grid[randomNumber][randomNumber2] = 0;
+            grid[randomNumber, randomNumber2] = 0;
         }
         
         print_grid();
