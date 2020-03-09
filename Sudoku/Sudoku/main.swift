ef74ae9f7f307bb1b492ea92761ede77b9822b68
Sakyawira <sakyawira@gmail.com>
2020-03-09 13:07:11 +1300

    Update main.swift

--- Sudoku/Sudoku/main.swift
+++ Sudoku/Sudoku/main.swift
@@ -10,7 +10,7 @@ import Foundation
 
 var sudoku = Sudoku();
 
-sudoku.generate_solvable_grid();
+// sudoku.generate_solvable_grid();
 
 print("Do you want to make your own grid?");
 print("0 :  No, 1 : Yes");
@@ -27,22 +27,50 @@ else
         print("Input the column you want to change");
         
         // Row
-        if let row = Int(readLine()!){
-            print(row);
+        var row : Int = 0;
+        if let irow = Int(readLine()!){
+            print(irow);
+            row = irow;
         }
         else{
             print("Please input a number between 0 - 8 !");
+            continue_s = nil;
             continue;
         }
         
         // Column
-        if let column = Int(readLine()!){
-            print(column);
+        var column : Int = 0;
+        if let icolumn = Int(readLine()!){
+            print(icolumn);
+            column = icolumn;
         }
         else{
             print("Please input a number between 0 - 8 !");
+            continue_s = nil;
             continue;
         }
+        
+        // Num
+        var number : Int = 0;
+        if let inumber = Int(readLine()!){
+            print(inumber);
+            number = inumber;
+        }
+        else{
+            print("Please input a number between 0 - 8 !");
+            continue_s = nil;
+            continue;
+        }
+        
+        if (sudoku.set_grid(Row: row, Col: column, Num: number))
+        {
+            print("Worked!");
+        }
+        else{
+            continue_s = nil;
+            continue;
+        }
+        
         continue_s = readLine()!;
     } while(continue_s != nil)
 }
