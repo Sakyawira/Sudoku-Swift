8367dd0c7a610f68dd9785eb976ec6e46d2e0887
Sakyawira <sakyawira@gmail.com>
2020-03-28 15:25:23 +1300

    add function headers to matrix function

--- Sudoku/Sudoku/Matrix.swift
+++ Sudoku/Sudoku/Matrix.swift
@@ -20,9 +20,21 @@ struct Matrix {
         self.columns = columns
         grid = Array(repeating: 0, count: rows * columns)
     }
+
+/***********************
+* indexIsValid(): check whether or not an index is within range
+* @parameter: Int, Int
+* @return: Bool (return false if index is out of range)
+********************/  
     func indexIsValid(row: Int, column: Int) -> Bool {
         return row >= 0 && row < rows && column >= 0 && column < columns
     }
+
+/***********************
+* printGrid(): print the matrix
+* @parameter: -
+* @return: -
+********************/  
     func printGrid(){
         for x in 0..<rows{
             var line = "";
@@ -36,6 +48,7 @@ struct Matrix {
         print("");
         //print(grid);
     }
+    
     subscript(row: Int, column: Int) -> Int {
         get {
             assert(indexIsValid(row: row, column: column),"Index out of range");
